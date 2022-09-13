0355010c1ea3a6d1bf833885504643f3d01f3d12
YellaReddy <m.yellareddy6@gmail.com>
2022-09-13 15:04:21 +0530

    used createStore method to create a store.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 import HelloWorld from "./HelloWorld";
+import {createStore} from 'redux';
+import reducer from './reducers';
+
+const initialState = {tech: "React"}
+
+const store = createStore(reducer, initialState);
 
 const App = () => {
-  const [tech] = useState("React");
-  return <HelloWorld tech={tech} />;
+  
+  return <HelloWorld tech={store.getState().tech} />;
 };
 
 export default App;
